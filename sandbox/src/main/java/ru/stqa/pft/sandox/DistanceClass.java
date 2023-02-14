e72500917a53ea126c20ef84129d89b2c490d535
Ekaterina Fatina <fatkinaek@gmail.com>
2023-02-14 16:26:53 +0300

    Задание 2 - вычисление расстояние м/у 2мя точками

--- sandbox/src/main/java/ru/stqa/pft/sandox/DistanceClass.java
+++ sandbox/src/main/java/ru/stqa/pft/sandox/DistanceClass.java
@@ -3,8 +3,10 @@ public class DistanceClass {
   public static void main(String[] args) {
     Point point1 = new Point(-1,4);
     Point point2 = new Point(10,15);
-    System.out.println("(Вызов функции) - Расстояние между точками на плоскости равно " + distance(point1, point2));
-    System.out.println("(Вызов метода) - Расстояние между точками на плоскости равно " + Point.distance(point1, point2));
+    System.out.println("Вычисляем расстояние при помощи функции:");
+    System.out.println("Расстояние между точками на плоскости равно " + distance(point1, point2));
+    System.out.println("Вычисляем расстояние при помощи метода, реализованного в классе Point:");
+    System.out.println("Расстояние между точками на плоскости равно " + Point.distance(point1, point2));
   }
 
   public static double distance(Point p1, Point p2) {
