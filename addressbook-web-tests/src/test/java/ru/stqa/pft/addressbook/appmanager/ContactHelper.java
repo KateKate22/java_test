7e7f304d13b368e38dacd4090e1af6377792b473
Ekaterina Fatina <fatkinaek@gmail.com>
2023-02-25 19:24:54 +0300

    Задание 5 (двухуровневая архитектура тестового набора)

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -0,0 +1,38 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactHelper extends HelperBase {
+
+  public ContactHelper(WebDriver wd) {
+    super(wd);
+  }
+
+  public void submitContactCreation() {
+    click(By.xpath("//div[@id='content']/form/input[21]"));
+  }
+
+  public void fillContactForm(ContactData contactData) {
+    type(By.name("firstname"), contactData.getName());
+    type(By.name("lastname"), contactData.getSurname());
+    type(By.name("nickname"), contactData.getNickname());
+    type(By.name("company"), contactData.getCompany());
+    type(By.name("home"), contactData.getHome());
+    type(By.name("mobile"), contactData.getMobile());
+    type(By.name("email"), contactData.getEmail());
+    type(By.name("email2"), contactData.getEmail2());
+    select(By.name("bday"), contactData.getBday());
+    select(By.name("bmonth"), contactData.getBmonth());
+    type(By.name("byear"), contactData.getByear());
+  }
+
+  public void initContactCreation() {
+    click(By.linkText("add new"));
+  }
+
+  public void returnToHomePage() {
+    click(By.linkText("home"));
+  }
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactCreationTests extends TestBase {
+
+  @Test
+  public void testContactCreation() throws Exception {
+    app.getContactHelper().initContactCreation();
+    app.getContactHelper().fillContactForm(new ContactData("Petr", "Petrov", "Wee", "Flower", "88142555555", "+79110000000", "peterpetrov@yandex.ru", "peter11petrov@gmai.com", "6", "May", "1980"));
+    app.getContactHelper().submitContactCreation();
+    app.getContactHelper().returnToHomePage();
+    app.getSessionHelper().logout();
+  }
+
+}
