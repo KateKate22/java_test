abb7681e4123ab9b2bb536bcf54f9742de39e717
Ekaterina Fatina <fatkinaek@gmail.com>
2023-03-20 09:59:34 +0300

    Задание 11 - проверка информации о контактах на главной странице (доработано)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDataInHomePageTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDataInHomePageTests.java
@@ -6,12 +6,12 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.Arrays;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 
-public class ContactPhoneTests extends TestBase {
+public class ContactDataInHomePageTests extends TestBase {
   String groupName; //  строковая переменная, хранящая имя группы, для последующей передачи в качестве параметра при создании контакта;
   @BeforeMethod
   public void ensurePreconditions() {
@@ -21,42 +21,34 @@ public class ContactPhoneTests extends TestBase {
         app.group().create(new GroupData().withName("test1"));
       }
       groupName = app.group().groupName(); // записываем в строковую переменную имя созданной либо уже имеющейся группы
-      app.contact().create(new ContactData("Petr", "Petrov", "Wee", "Flower", "88142555555", "+79110000000", "555555", "peterpetrov@yandex.ru", "peter11petrov@gmai.com", "6", "May", "1980", groupName));
+      app.contact().create(new ContactData().setName("Petr").setSurname("Petrov").setNickname("Wee")
+              .setCompany("Flower").setAddress("Lenina street 33-8").setHome("88142555555").setMobile("+79110000000").
+              setWork("555555").setEmail("peterpetrov@yandex.ru").setEmail2("peter11petrov@gmail.com").
+              setEmail3("peter1234petrov@gmail.com").setBday("6").setBmonth("May").setByear("1980").setGroup(groupName));
       app.goTo().homePage();
     }
     app.goTo().homePage();
   }
   @Test
-  public void testContactPhones() {
+  public void testContactDataInHomePage() {
     ContactData contact = app.contact().all().iterator().next();
     ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
-    MatcherAssert.assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
-  }
-
-  @Test
-  public void testContactEmails() {
-    ContactData contact = app.contact().all().iterator().next();
-    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
-    MatcherAssert.assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
-  }
-
-  @Test
-  public void testContactAddress() {
-    ContactData contact = app.contact().all().iterator().next();
-    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+    // проверяем совпадение адреса на главной странице и форме редактирования:
     MatcherAssert.assertThat(contact.getAddress(), equalTo(contactInfoFromEditForm.getAddress()));
-  }
+    // проверяем совпадение эмейлов на главной странице и форме редактирования:
+    MatcherAssert.assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
+    // проверяем совпадение телефонов на главной странице и форме редактирования:
+    MatcherAssert.assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
 
+  }
   public String mergePhones(ContactData contact) {
-    return Arrays.asList(contact.getHome(), contact.getMobile(), contact.getWork())
-            .stream().filter((s) -> ! s.equals(""))
-            .map(ContactPhoneTests::cleaned)
+    return Stream.of(contact.getHome(), contact.getMobile(), contact.getWork()).filter((s) -> ! s.equals(""))
+            .map(ContactDataInHomePageTests::cleaned)
             .collect(Collectors.joining("\n"));
   }
 
   public String mergeEmails(ContactData contact) {
-    return Arrays.asList(contact.getEmail(), contact.getEmail2(), contact.getEmail3())
-            .stream().filter((s) -> ! s.equals(""))
+    return Stream.of(contact.getEmail(), contact.getEmail2(), contact.getEmail3()).filter((s) -> ! s.equals(""))
             .collect(Collectors.joining("\n"));
   }
 
