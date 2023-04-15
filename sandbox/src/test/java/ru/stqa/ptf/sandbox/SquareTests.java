865a6a85541aaf817eb30f299bbe34b3aee76331
Ekaterina Fatina <fatkinaek@gmail.com>
2023-04-15 11:41:20 +0300

    Испорчен тест для проверки Jenkins

--- sandbox/src/test/java/ru/stqa/ptf/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/ptf/sandbox/SquareTests.java
@@ -9,6 +9,6 @@ public class SquareTests {
   @Test
   public void testArea() {
     Square s = new Square(5);
-    Assert.assertEquals(s.calcArea(), 25.0);
+    Assert.assertEquals(s.calcArea(), 20.0);
   }
 }
